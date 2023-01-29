c6c6e6ab08c2c2a76ef37a8edd8b7dbfa82fd00e
UlulAzmi111 <azmiulul1453@gmail.com>
2023-01-29 22:26:13 +0700

    Memodifikasi file untuk video 8 -> Penggunaan State -> (after rendering)

--- src/pages/Tentang.js
+++ src/pages/Tentang.js
@@ -1,10 +1,25 @@
+import { useState } from "react";
+
 function Tentang() {
+
+  const [count, setCount] = useState(0);
+
+  function tambah() {
+    setCount(count+1)
+  }
+
+  function kurang() {
+    if (count > 0) {
+      setCount(count-1)
+    }
+  }
+
     return (
       <div className="App">
-        <h1>Tentang SMK REVIT</h1>
+        <h1>Tentang SMK REVIT counter : {count}</h1>
         <p>isi tentang smk revit</p>
-        <button type="button" className="btn btn-primary">Primary</button>
-        <button type="button" className="btn btn-success">Success</button>
+        <button type="button" onClick={tambah} className="btn btn-primary">Tambah</button>
+        <button type="button" onClick={kurang} className="btn btn-success">Kurang</button>
       </div>
     );
   }
